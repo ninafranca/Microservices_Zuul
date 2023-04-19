eede06e8dd5d440349a6b5bcd6778b5760a7623d
Nina Spinelli <ninafspin@gmail.com>
2023-04-19 16:29:00 -0300

    zuul server configurado

---
+++ src/main/java/com/zuul/zuulServer/Filters/PostElapsedTimeFilter.java
@@ -0,0 +1,45 @@
+package com.zuul.zuulServer.Filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PostElapsedTimeFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PostElapsedTimeFilter.class);
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("Going into post filter");
+        Long startingTime = (Long) request.getAttribute("startingTime");
+        Long finishingTime = System.currentTimeMillis();
+        Long elapsedTime = startingTime - finishingTime;
+        log.info(String.format("Elapsed time in seconds %s", elapsedTime.doubleValue()/1000.00));
+        //log.info(String.format("Elapsed time in milliseconds %ms", elapsedTime));
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+}
---
+++ src/main/java/com/zuul/zuulServer/Filters/PreElapsedTimeFilter.java
@@ -0,0 +1,42 @@
+package com.zuul.zuulServer.Filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PreElapsedTimeFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PreElapsedTimeFilter.class);
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s request routed to %s", request.getMethod(), request.getRequestURL().toString()));
+        Long startingTime = System.currentTimeMillis();
+        request.setAttribute("startingTime", startingTime);
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+}
